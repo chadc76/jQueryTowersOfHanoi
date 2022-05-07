dc268aaed62277dacb97d8a79557860a0a30efd7
Chad Cunningham <chadcunningham7676@gmail.com>
2022-05-07 12:46:00 -0400

    Added setup towers method

--- dst/main.js
+++ dst/main.js
@@ -140,7 +140,27 @@
 /***/ (function(module, exports) {
 
 	class View {
-	  constructor(game, $el) {}
+	  constructor(game, $el) {
+	    this.game = game;
+	    this.$el = $el;
+
+	    this.setupTowers();
+	  }
+
+	  setupTowers() {
+	    const $ul1 = $("<ul>");
+	    const $ul2 = $("<ul>");
+	    const $ul3 = $("<ul>");
+
+	    for (let i = 0; i < 3; i++) {
+	      let $li = $("<li>")
+	      $li.data("pos", [0, i]);
+
+	      $ul1.append($li);
+	    }
+
+	    this.$el.append($ul1, $ul2, $ul3)
+	  }
 	}
 
 	module.exports = View;
